cf4eacbad88fc75452706e3eb9048f2699e1bf11
Mistralys <s.mordziol@gmail.com>
2019-09-30 08:32:39 +0200

    Added methods to relativize file or folder paths

--- src/FileHelper.php
+++ src/FileHelper.php
@@ -902,4 +902,79 @@ class FileHelper
         
         return round($size);
     }
+   
+   /**
+    * Makes a path relative using a folder depth: will reduce the
+    * length of the path so that only the amount of folders defined
+    * in the <code>$depth</code> attribute are shown below the actual
+    * folder or file in the path.
+    *  
+    * @param string  $path The absolute or relative path
+    * @param int $depth The folder depth to reduce the path to
+    * @return string
+    */
+    public static function relativizePathByDepth(string $path, int $depth=2) : string
+    {
+        $path = self::normalizePath($path);
+        
+        $tokens = explode('/', $path);
+        $tokens = array_filter($tokens); // remove empty entries (trailing slash for example)
+        $tokens = array_values($tokens); // re-index keys
+        
+        if(empty($tokens)) {
+            return '';
+        }
+        
+        // remove the drive if present
+        if(strstr($tokens[0], ':')) {
+            array_shift($tokens);
+        }
+        
+        // path was only the drive
+        if(count($tokens) == 0) {
+            return '';
+        }
+
+        // the last element (file or folder)
+        $target = array_pop($tokens);
+        
+        // reduce the path to the specified depth
+        $length = count($tokens);
+        if($length > $depth) {
+            $tokens = array_slice($tokens, $length-$depth);
+        }
+
+        // append the last element again
+        $tokens[] = $target;
+        
+        return trim(implode('/', $tokens), '/');
+    }
+    
+   /**
+    * Makes the specified path relative to another path,
+    * by removing one from the other if found. Also 
+    * normalizes the path to use forward slashes. 
+    * 
+    * Example:
+    * 
+    * <pre>
+    * relativizePath('c:\some\folder\to\file.txt', 'c:\some\folder');
+    * </pre>
+    * 
+    * Result: <code>to/file.txt</code>
+    * 
+    * @param string $path
+    * @param string $relativeTo
+    * @return string
+    */
+    public static function relativizePath(string $path, string $relativeTo) : string
+    {
+        $path = self::normalizePath($path);
+        $relativeTo = self::normalizePath($relativeTo);
+        
+        $relative = str_replace($relativeTo, '', $path);
+        $relative = trim($relative, '/');
+        
+        return $relative;
+    }
 }
---
+++ tests/testsuites/FileHelper.php
@@ -0,0 +1,85 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use AppUtils\FileHelper;
+
+final class FileHelperTest extends TestCase
+{
+    public function test_relativizePathByDepth()
+    {
+        $tests = array(
+            array(
+                'path' => 'c:\\',
+                'result' => '',
+                'depth' => 2
+            ),
+            array(
+                'path' => 'f:\file.txt',
+                'result' => 'file.txt',
+                'depth' => 2
+            ),
+            array(
+                'path' => 'c:\path\to\file.txt',
+                'result' => 'path/to/file.txt',
+                'depth' => 2
+            ),
+            array(
+                'path' => 'c:\path\to\some\other\file.txt',
+                'result' => 'some/other/file.txt',
+                'depth' => 2
+            ),
+            array(
+                'path' => 'g:\path\to\folder',
+                'result' => 'path/to/folder',
+                'depth' => 2
+            ),
+            array(
+                'path' => 'g:\path\to\folder\\',
+                'result' => 'path/to/folder',
+                'depth' => 2
+            ),
+            array(
+                'path' => '/path/to/folder/and/even/further/down/the/road/',
+                'result' => 'down/the/road',
+                'depth' => 2
+            ),
+            array(
+                'path' => '/path/to/folder/and/even/further/down/the/road/',
+                'result' => 'and/even/further/down/the/road',
+                'depth' => 5
+            ),
+        );
+        
+        foreach($tests as $def)
+        {
+            $this->assertEquals($def['result'], FileHelper::relativizePathByDepth($def['path'], $def['depth']));
+        }
+    }
+    
+    public function test_relativizePath()
+    {
+        $tests = array(
+            array(
+                'path' => 'c:\test\folder\here',
+                'relativeTo' => 'c:\test',
+                'result' => 'folder/here',
+            ),
+            array(
+                'path' => 'f:\file.txt',
+                'relativeTo' => 'f:',
+                'result' => 'file.txt',
+            ),
+            array(
+                'path' => 'g:\file.txt',
+                'relativeTo' => 'f:\file.txt',
+                'result' => 'g:/file.txt',
+            ),
+        );
+        
+        foreach($tests as $def)
+        {
+            $this->assertEquals($def['result'], FileHelper::relativizePath($def['path'], $def['relativeTo']));
+        }
+    }
+}
