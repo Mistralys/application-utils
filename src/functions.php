1ba0b3cb064cab86613cb7a92ac9a4bb6b272a6b
Mistralys <s.mordziol@gmail.com>
2019-10-17 10:57:48 +0200

    Auto-Register localization source for the package.

--- src/functions.php
+++ src/functions.php
@@ -49,3 +49,30 @@ function t()
     // simulate the translation function
     return call_user_func_array('sprintf', $args);
 }
+
+/**
+ * Initializes the utilities: this is called automatically
+ * because this file is included in the files list in the
+ * composer.json, guaranteeing it is always loaded.
+ */
+function init()
+{
+    if(!class_exists('\AppLocalize\Localization')) {
+        return;
+    }
+    
+    $installFolder = realpath(__DIR__.'/../');
+    
+    // Register the classes as a localization source,
+    // so they can be found, and use the bundled localization
+    // files.
+    \AppLocalize\Localization::addSourceFolder(
+        'application-utils',
+        'Application Utils Package',
+        'Composer Packages',
+        $installFolder.'/localization',
+        $installFolder.'/src'
+    );
+}
+
+init();
