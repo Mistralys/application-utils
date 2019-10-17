24ef1d066b22c69ca25fd66eed25f073ebdad26d
Mistralys <s.mordziol@gmail.com>
2019-10-17 10:57:28 +0200

    Added the localization editor UI.

---
+++ localization/index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Translation UI for the localizable strings in the package.
+ *
+ * @package Application Utils
+ * @subpackage Examples
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+    
+    $root = __DIR__;
+    
+    $autoload = realpath($root.'/../vendor/autoload.php');
+    
+    // we need the autoloader to be present
+    if(!file_exists($autoload)) {
+        die('<b>ERROR:</b> Autoloader not present. Run composer update first.');
+    }
+    
+    /**
+     * The composer autoloader
+     */
+    require_once $autoload;
+    
+    // add the locales we wish to manage (en_UK is always present)
+    \AppLocalize\Localization::addAppLocale('de_DE');
+    \AppLocalize\Localization::addAppLocale('fr_FR');
+    
+    // has to be called last after all sources and locales have been configured
+    \AppLocalize\Localization::configure($root.'/storage.json', '');
+    
+    // create the editor UI and start it
+    $editor = \AppLocalize\Localization::createEditor();
+    $editor->display();
