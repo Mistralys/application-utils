33d9392a660482282d78998d35db5c5876022736
Mistralys <s.mordziol@gmail.com>
2019-11-26 19:49:09 +0100

    Added the getStringOption() and getBoolOption() methods.

--- src/Traits/Optionable.php
+++ src/Traits/Optionable.php
@@ -38,6 +38,43 @@ trait Traits_Optionable
         return $default;
     }
     
+   /**
+    * Enforces that the option value is a string. Scalar 
+    * values are converted to string, and non-scalar values
+    * are converted to an empty string.
+    * 
+    * @param string $name
+    * @return string
+    */
+    public function getStringOption(string $name) : string
+    {
+        $value = $this->getOption($name, false);
+        
+        if(is_scalar($value)) {
+            return (string)$value;
+        }
+        
+        return '';
+    }
+    
+   /**
+    * Treats the option value as a boolean value: will return
+    * true if the value actually is a boolean true.
+    * 
+    * NOTE: boolean string representations are not accepted.
+    * 
+    * @param string $name
+    * @return bool
+    */
+    public function getBoolOption(string $name) : bool
+    {
+        if($this->getOption($name) === true) {
+            return true;
+        }
+        
+        return false;
+    }
+    
     public function hasOption($name) : bool
     {
         if(!isset($this->options)) {
