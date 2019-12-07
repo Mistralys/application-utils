8c6a0c978715b99592b1fef888a6f33d845d9ba5
Mistralys <s.mordziol@gmail.com>
2019-12-07 17:50:22 +0100

    Added some more tests.

--- tests/testsuites/URLInfo.php
+++ tests/testsuites/URLInfo.php
@@ -3,6 +3,7 @@
 use PHPUnit\Framework\TestCase;
 
 use AppUtils\URLInfo;
+use function AppUtils\parseURL;
 
 final class URLInfoTest extends TestCase
 {
@@ -216,5 +217,129 @@ final class URLInfoTest extends TestCase
             $this->assertEquals($test['isPhone'], $info->isPhoneNumber(), $test['label'].' Error: '.$info->getErrorMessage());
             $this->assertEquals($test['normalized'], $info->getNormalized(), $test['label'].' Error: '.$info->getErrorMessage());
         }
+    }
+    
+    public function test_globalFunction()
+    {
+        $info = parseURL('http://foo.com');
+        
+        $this->assertInstanceOf(URLInfo::class, $info);
+    }
+    
+    public function test_arrayAccess()
+    {
+        $info = parseURL('http://user:pass@foo.com:1234/path/to/page/index.html#fragment');        
+        
+        $this->assertEquals('http', $info['scheme']);
+        $this->assertEquals('user', $info['user']);
+        $this->assertEquals('pass', $info['pass']);
+        $this->assertEquals('foo.com', $info['host']);
+        $this->assertSame(1234, $info['port']);
+        $this->assertEquals('/path/to/page/index.html', $info['path']);
+        $this->assertEquals('fragment', $info['fragment']);
+    }
+    
+    public function test_arrayAccess_empty()
+    {
+        $info = parseURL('//foo.com');
+        
+        $this->assertSame('', $info['scheme']);
+        $this->assertSame('', $info['user']);
+        $this->assertSame('', $info['pass']);
+        $this->assertSame(-1, $info['port']);
+        $this->assertSame('', $info['path']);
+        $this->assertSame('', $info['fragment']);
+    }
+
+    public function test_scheme()
+    {
+        $tests = array(
+            array(
+                'label' => 'Regular HTTP url',
+                'url' => 'http://foo.com',
+                'expected' => 'http',
+                'hasScheme' => true,
+                'isSecure' => false
+            ),
+            array(
+                'label' => 'Regular HTTPS url',
+                'url' => 'https://foo.com',
+                'expected' => 'https',
+                'hasScheme' => true,
+                'isSecure' => true
+            ),
+            array(
+                'label' => 'Regular FTP url',
+                'url' => 'ftp://foo.com',
+                'expected' => 'ftp',
+                'hasScheme' => true,
+                'isSecure' => false
+            ),
+            array(
+                'label' => 'Schemeless but valid URL',
+                'url' => '//foo.com',
+                'expected' => '',
+                'hasScheme' => false,
+                'isSecure' => false
+            ),
+            array(
+                'label' => 'Invalid URL',
+                'url' => 'foo.com',
+                'expected' => '',
+                'hasScheme' => false,
+                'isSecure' => false
+            )
+        );
+
+        foreach($tests as $test)
+        {
+            $info = parseURL($test['url']);
+            
+            $this->assertEquals($test['expected'], $info->getScheme(), $test['label']);
+            $this->assertEquals($test['hasScheme'], $info->hasScheme(), $test['label']);
+            $this->assertEquals($test['isSecure'], $info->isSecure(), $test['label']);
+        }
+    }
+    
+    public function test_port()
+    {
+        $tests = array(
+            array(
+                'label' => 'No port specified',
+                'url' => 'http://foo.com',
+                'expected' => -1,
+                'hasPort' => false,
+            ),
+            array(
+                'label' => 'Port specified',
+                'url' => 'http://foo.com:3120',
+                'expected' => 3120,
+                'hasPort' => true,
+            )
+        );
+        
+        foreach($tests as $test)
+        {
+            $info = parseURL($test['url']);
+            
+            $this->assertSame($test['expected'], $info->getPort(), $test['label']);
+            $this->assertEquals($test['hasPort'], $info->hasPort(), $test['label']);
+        }
+    }
+    
+   /**
+    * Ensure that the same URLs, but with a different order of parameters
+    * have the same hash (which is generated from the normalized URL).
+    */
+    public function test_getHash()
+    {
+        $url1 = 'http://foo.com?param1=foo&param2=bar&param3=dog';
+        $url2 = 'http://foo.com?param3=dog&param1=foo&param2=bar';
+        
+        $info1 = parseURL($url1);
+        $info2 = parseURL($url2);
+        
+        $this->assertEquals($info1->getNormalized(), $info2->getNormalized(), 'The normalized URLs should match.');
+        $this->assertEquals($info1->getHash(), $info2->getHash(), 'The hashes should match.');
     }
 }
