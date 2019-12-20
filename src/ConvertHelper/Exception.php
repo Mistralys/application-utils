2b0cd30ce6e9a8852d1d6292f6051dc2b8081d6b
Mistralys <s.mordziol@gmail.com>
2019-12-20 13:39:35 +0100

    Added docblocks.

--- src/ConvertHelper/Exception.php
+++ src/ConvertHelper/Exception.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * File containing the {@see AppUtils\ConvertHelper_Exception} class.
+ * 
+ * @package Application Utils
+ * @subpackage ConvertHelper
+ * @see AppUtils\ConvertHelper_Exception
+ */
 
 namespace AppUtils;
 
+/**
+ * Exception class used for all convert helper exceptions.
+ *
+ * @package Application Utils
+ * @subpackage ConvertHelper
+ */
 class ConvertHelper_Exception extends BaseException
 {
 }
