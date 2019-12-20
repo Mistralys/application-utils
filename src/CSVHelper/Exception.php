db4a744db81eb5b5ec17d9a3d65db75bf1d7d5fd
Mistralys <s.mordziol@gmail.com>
2019-12-20 13:40:47 +0100

    Added docblocks.

--- src/CSVHelper/Exception.php
+++ src/CSVHelper/Exception.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * File containing the {@see AppUtils\CSVHelper_Exception} class.
+ *
+ * @package Application Utils
+ * @subpackage CSVHelper
+ * @see AppUtils\CSVHelper_Exception
+ */
 
 namespace AppUtils;
 
+/**
+ * Exception class used for all CSV helper exceptions.
+ *
+ * @package Application Utils
+ * @subpackage CSVHelper
+ */
 class CSVHelper_Exception extends BaseException
 {
 }
