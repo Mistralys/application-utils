f802e2071ba347ac2a1167126475daa6eb2a07e8
Mistralys <s.mordziol@gmail.com>
2019-12-22 20:49:54 +0100

    Simplified the code.

--- src/ConvertHelper/DurationConverter.php
+++ src/ConvertHelper/DurationConverter.php
@@ -36,6 +36,31 @@ class ConvertHelper_DurationConverter
     */
     protected $dateTo;
     
+   /**
+    * @var bool
+    */
+    protected $future = false;
+    
+   /**
+    * @var string
+    */
+    protected $interval = '';
+    
+   /**
+    * @var int
+    */
+    protected $difference = 0;
+    
+   /**
+    * @var int
+    */
+    protected $dateDiff = 0;
+    
+   /**
+    * @var array
+    */
+    protected static $texts;
+    
     public function __construct()
     {
     }
@@ -82,7 +107,135 @@ class ConvertHelper_DurationConverter
     */
     public function convert() : string
     {
-        if(!isset($this->dateFrom)) 
+        $this->initTexts();
+        $this->resolveCalculations();
+        
+        $epoch = 'past';
+        $key = 'singular';
+        if($this->dateDiff > 1) {
+            $key = 'plural';
+        }
+        
+        if($this->future) {
+            $epoch = 'future'; 
+        }
+        
+        $key .= '-'.$epoch;
+        
+        $text = self::$texts[$this->interval][$key];
+        
+        return str_replace('$value', $this->dateDiff, $text);
+    }
+    
+    protected function initTexts()
+    {
+        if(isset(self::$texts)) {
+            return;
+        }
+        
+        self::$texts = array(
+            'y' => array(
+                'singular-future' => t('In one year'),
+                'plural-future' => t('In %1s years', '$value'),
+                'singular-past' => t('One year ago'),
+                'plural-past' => t('%1s years ago', '$value')
+            ),
+            'm' => array(
+                'singular-future' => t('In one month'),
+                'plural-future' => t('In %1s months', '$value'),
+                'singular-past' => t('One month ago'),
+                'plural-past' => t('%1s months ago', '$value')
+            ),
+            'ww' => array(
+                'singular-future' => t('In one week'),
+                'plural-future' => t('In %1s weeks', '$value'),
+                'singular-past' => t('One week ago'),
+                'plural-past' => t('%1s weeks ago', '$value')
+            ),
+            'd' => array(
+                'singular-future' => t('In one day'),
+                'plural-future' => t('In %1s days', '$value'),
+                'singular-past' => t('One day ago'),
+                'plural-past' => t('%1s days ago', '$value')
+            ),
+            'h' => array(
+                'singular-future' => t('In one hour'),
+                'plural-future' => t('In %1s hours', '$value'),
+                'singular-past' => t('One hour ago'),
+                'plural-past' => t('%1s hours ago', '$value')
+            ),
+            'n' => array(
+                'singular-future' => t('In one minute'),
+                'plural-future' => t('In %1s minutes', '$value'),
+                'singular-past' => t('One minute ago'),
+                'plural-past' => t('%1s minutes ago', '$value')
+            ),
+            's' => array(
+                'singular-future' => t('In one second'),
+                'plural-future' => t('In %1s seconds', '$value'),
+                'singular-past' => t('One second ago'),
+                'plural-past' => t('%1s seconds ago', '$value')
+            )
+        );
+    }
+    
+    protected function convert_minute() : int
+    {
+        return (int)floor($this->difference / 60);
+    }
+    
+    protected function convert_hour() : int
+    {
+        return (int)floor($this->difference / 60 / 60);
+    }
+    
+    protected function convert_week() : int
+    {
+        return (int)floor($this->difference / 60 / 60 / 24 / 7);
+    }
+    
+    protected function convert_day() : int
+    {
+        return (int)floor($this->difference / 60 / 60 / 24);
+    }
+    
+    protected function convert_year() : int
+    {
+        return (int)floor($this->difference / 60 / 60 / 24 / 365);
+    }
+    
+    protected function convert_month() : int
+    {
+        $months_difference = (int)floor($this->difference / 60 / 60 / 24 / 29);
+        
+        $hour = (int)date("H", $this->dateFrom);
+        $min = (int)date("i", $this->dateFrom);
+        $sec = (int)date("s", $this->dateFrom);
+        $month = (int)date("n", $this->dateFrom);
+        $day = (int)date("j", $this->dateTo);
+        $year = (int)date("Y", $this->dateFrom);
+        
+        while(mktime($hour, $min, $sec, $month + ($months_difference), $day, $year) < $this->dateTo)
+        {
+            $months_difference++;
+        }
+        
+        $datediff = $months_difference;
+        
+        // We need this in here because it is possible
+        // to have an 'm' interval and a months
+        // difference of 12 because we are using 29 days
+        // in a month
+        if ($datediff == 12) {
+            $datediff--;
+        }
+        
+        return $datediff;
+    }
+    
+    protected function resolveCalculations() : void
+    {
+        if(!isset($this->dateFrom))
         {
             throw new ConvertHelper_Exception(
                 'No date from has been specified.',
@@ -92,54 +245,52 @@ class ConvertHelper_DurationConverter
         }
         
         // no date to set? Assume we want to use today.
-        if(!isset($this->dateTo)) 
+        if(!isset($this->dateTo))
         {
             $this->dateTo = time();
         }
         
-        // Calculate the difference in seconds betweeen
-        // the two timestamps
-        
-        $difference = $this->dateTo - $this->dateFrom;
-        
-        $interval = "";
-        
-        $future = false;
-        if($difference < 0) 
-        {
-            $difference = $difference * -1;
-            $future = true;
-        }
-        
+        $this->difference = $this->resolveDifference();
+        $this->interval = $this->resolveInterval();
+        $this->dateDiff = $this->resolveDateDiff();
+    }
+    
+    protected function resolveInterval() : string
+    {
         // If difference is less than 60 seconds,
         // seconds is a good interval of choice
         
-        if ($difference < 60) {
-            $interval = "s";
+        if ($this->difference < 60) 
+        {
+            return "s";
         }
         
         // If difference is between 60 seconds and
         // 60 minutes, minutes is a good interval
-        elseif ($difference >= 60 && $difference < 60 * 60) {
-            $interval = "n";
+        if ($this->difference >= 60 && $this->difference < 60 * 60) 
+        {
+            return "n";
         }
         
         // If difference is between 1 hour and 24 hours
         // hours is a good interval
-        elseif ($difference >= 60 * 60 && $difference < 60 * 60 * 24) {
-            $interval = "h";
+        if ($this->difference >= 60 * 60 && $this->difference < 60 * 60 * 24) 
+        {
+            return "h";
         }
         
         // If difference is between 1 day and 7 days
         // days is a good interval
-        elseif ($difference >= 60 * 60 * 24 && $difference < 60 * 60 * 24 * 7) {
-            $interval = "d";
+        if ($this->difference >= 60 * 60 * 24 && $this->difference < 60 * 60 * 24 * 7) 
+        {
+            return "d";
         }
         
         // If difference is between 1 week and 30 days
         // weeks is a good interval
-        elseif ($difference >= 60 * 60 * 24 * 7 && $difference < 60 * 60 * 24 * 30) {
-            $interval = "ww";
+        if ($this->difference >= 60 * 60 * 24 * 7 && $this->difference < 60 * 60 * 24 * 30) 
+        {
+            return "ww";
         }
         
         // If difference is between 30 days and 365 days
@@ -147,8 +298,9 @@ class ConvertHelper_DurationConverter
         // applies, if the 29th February happens to exist
         // between your 2 dates, the function will return
         // the 'incorrect' value for a day
-        elseif ($difference >= 60 * 60 * 24 * 30 && $difference < 60 * 60 * 24 * 365) {
-            $interval = "m";
+        if ($this->difference >= 60 * 60 * 24 * 30 && $this->difference < 60 * 60 * 24 * 365) 
+        {
+            return "m";
         }
         
         // If difference is greater than or equal to 365
@@ -157,12 +309,32 @@ class ConvertHelper_DurationConverter
         // 2008 passing in 29th April 2007. It will return
         // 1 year ago when in actual fact (yawn!) not quite
         // a year has gone by
-        elseif ($difference >= 60 * 60 * 24 * 365) {
-            $interval = "y";
+        if ($this->difference >= 60 * 60 * 24 * 365) 
+        {
+            return "y";
         }
         
-        $result = '';
+        return ""; 
+    }
+    
+    protected function resolveDifference() : int
+    {
+        // Calculate the difference in seconds betweeen
+        // the two timestamps
         
+        $difference = $this->dateTo - $this->dateFrom;
+        
+        if($difference < 0)
+        {
+            $difference = $difference * -1;
+            $this->future = true;
+        }
+        
+        return $difference;
+    }
+    
+    protected function resolveDateDiff() : int
+    {
         // Based on the interval, determine the
         // number of units between the two dates
         // From this point on, you would be hard
@@ -170,94 +342,28 @@ class ConvertHelper_DurationConverter
         // this function and DateDiff. If the $datediff
         // returned is 1, be sure to return the singular
         // of the unit, e.g. 'day' rather 'days'
-        switch ($interval)
+        switch ($this->interval)
         {
             case "m":
-                $months_difference = (int)floor($difference / 60 / 60 / 24 / 29);
-                $hour = (int)date("H", $this->dateFrom);
-                $min = (int)date("i", $this->dateFrom);
-                $sec = (int)date("s", $this->dateFrom);
-                $month = (int)date("n", $this->dateFrom);
-                $day = (int)date("j", $this->dateTo);
-                $year = (int)date("Y", $this->dateFrom);
-                
-                while(mktime($hour, $min, $sec, $month + ($months_difference), $day, $year) < $this->dateTo)
-                {
-                    $months_difference++;
-                }
-                
-                $datediff = $months_difference;
-                
-                // We need this in here because it is possible
-                // to have an 'm' interval and a months
-                // difference of 12 because we are using 29 days
-                // in a month
-                if ($datediff == 12) {
-                    $datediff--;
-                }
-                
-                if($future) {
-                    $result = ($datediff == 1) ? t('In one month', $datediff) : t('In %1s months', $datediff);
-                } else {
-                    $result = ($datediff == 1) ? t('One month ago', $datediff) : t('%1s months ago', $datediff);
-                }
-                break;
+                return $this->convert_month();
                 
             case "y":
-                $datediff = floor($difference / 60 / 60 / 24 / 365);
-                if($future) {
-                    $result = ($datediff == 1) ? t('In one year', $datediff) : t('In %1s years', $datediff);
-                } else {
-                    $result = ($datediff == 1) ? t('One year ago', $datediff) : t('%1s years ago', $datediff);
-                }
-                break;
+                return $this->convert_year();
                 
             case "d":
-                $datediff = floor($difference / 60 / 60 / 24);
-                if($future) {
-                    $result = ($datediff == 1) ? t('In one day', $datediff) : t('In %1s days', $datediff);
-                } else {
-                    $result = ($datediff == 1) ? t('One day ago', $datediff) : t('%1s days ago', $datediff);
-                }
-                break;
+                return $this->convert_day();
                 
             case "ww":
-                $datediff = floor($difference / 60 / 60 / 24 / 7);
-                if($future) {
-                    $result = ($datediff == 1) ? t('In one week', $datediff) : t('In %1s weeks', $datediff);
-                } else {
-                    $result = ($datediff == 1) ? t('One week ago', $datediff) : t('%1s weeks ago', $datediff);
-                }
-                break;
+                return $this->convert_week();
                 
             case "h":
-                $datediff = floor($difference / 60 / 60);
-                if($future) {
-                    $result = ($datediff == 1) ? t('In one hour', $datediff) : t('In %1s hours', $datediff);
-                } else {
-                    $result = ($datediff == 1) ? t('One hour ago', $datediff) : t('%1s hours ago', $datediff);
-                }
-                break;
+                return $this->convert_hour();
                 
             case "n":
-                $datediff = floor($difference / 60);
-                if($future) {
-                    $result = ($datediff == 1) ? t('In one minute', $datediff) : t('In %1s minutes', $datediff);
-                } else {
-                    $result = ($datediff == 1) ? t('One minute ago', $datediff) : t('%1s minutes ago', $datediff);
-                }
-                break;
-                
-            case "s":
-                $datediff = $difference;
-                if($future) {
-                    $result = ($datediff == 1) ? t('In one second', $datediff) : t('In %1s seconds', $datediff);
-                } else {
-                    $result = ($datediff == 1) ? t('One second ago', $datediff) : t('%1s seconds ago', $datediff);
-                }
-                break;
+                return $this->convert_minute();
         }
         
-        return $result;
+        // seconds
+        return $this->difference;
     }
 }
