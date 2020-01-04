187ba9bf947adc6bf37caf93ac3aac669b79d9c6
Mistralys <s.mordziol@gmail.com>
2020-01-04 19:23:28 +0100

    Added URL filtering class, to move the code out of URLInfo.

---
+++ src/URLInfo/Filter.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * File containing the {@see AppUtils\URLInfo_Filter} class.
+ *
+ * @package Application Utils
+ * @subpackage URLInfo
+ * @see AppUtils\URLInfo_Filter
+ */
+
+declare(strict_types=1);
+
+namespace AppUtils;
+
+/**
+ * Static class with utility methods to handle filtering 
+ * an URL string, by removing any unwanted special characters 
+ * and the like.
+ *
+ * @package Application Utils
+ * @subpackage URLInfo
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+class URLInfo_Filter
+{
+    public static function filter(string $url) : string
+    {
+        // fix ampersands if it comes from HTML
+        $url = str_replace('&amp;', '&', $url);
+        
+        // we remove any control characters from the URL, since these
+        // may be copied when copy+pasting from word or pdf documents
+        // for example.
+        $url = \AppUtils\ConvertHelper::stripControlCharacters($url);
+        
+        // fix the pesky unicode hyphen that looks like a regular hyphen,
+        // but isn't and can cause all sorts of problems
+        $url = str_replace('‐', '-', $url);
+        
+        // remove newlines and tabs
+        $url = str_replace(array("\n", "\r", "\t"), '', $url);
+        
+        $url = trim($url);
+        
+        return $url;
+    }
+}
