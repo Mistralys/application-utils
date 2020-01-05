18ea489d106f8cbe4cc27e3c551945b98c8909bb
Mistralys <s.mordziol@gmail.com>
2020-01-05 18:16:59 +0100

    Added a test for XML to string conversion.

---
+++ tests/testsuites/XMLHelper.php
@@ -0,0 +1,64 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use AppUtils\XMLHelper;
+use AppUtils\XMLHelper_Exception;
+
+final class XMLHelperTest extends TestCase
+{
+    /**
+     * @see XMLHelper::convertString()
+     */
+    public function test_convertString()
+    {
+        $xmlString = 
+'<?xml version="1.0" encoding="UTF-8"?>
+<root type="root">
+    <title>Title</title>
+    <items>
+        <item name="Item 1"/>
+        <item name="Item 2"/>
+    </items>
+    <self_closing/>
+    <empty></empty>
+</root>';
+        
+        $result = XMLHelper::convertString($xmlString);
+
+        $array = $result->toArray();
+        
+        $expected = array(
+            '@attributes' => array(
+                'type' => 'root'
+            ),
+            'title' => array(
+                '@attributes' => array(),
+                '@text' => 'Title'
+            ),
+            'items' => array(
+                '@attributes' => array(),
+                'item' => array(
+                    array(
+                        '@attributes' => array(
+                            'name' => 'Item 1'
+                        )
+                    ),
+                    array(
+                        '@attributes' => array(
+                            'name' => 'Item 2'
+                        )
+                    )
+                )
+            ),
+            'self_closing' => array(
+                '@attributes' => array()
+            ),
+            'empty' => array(
+                '@attributes' => array()
+            )
+        );
+        
+        $this->assertEquals($expected, $array);
+    }
+}
