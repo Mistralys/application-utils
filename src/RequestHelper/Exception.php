32fb813eab1e97d333e7bffae21f671c782bf81a
Mistralys <s.mordziol@gmail.com>
2020-01-10 17:41:08 +0100

    Added the possibility to access the response.

--- src/RequestHelper/Exception.php
+++ src/RequestHelper/Exception.php
@@ -1,7 +1,44 @@
 <?php
+/**
+ * File containing the {@link RequestHelper_Exception} class.
+ * @package Application Utils
+ * @subpackage RequestHelper
+ * @see RequestHelper_Exception
+ */
+
+declare(strict_types=1);
 
 namespace AppUtils;
 
+/**
+ * Request helper exception class: all exceptions in the
+ * request helper are of this type. If available, this
+ * allows accessing the request response if the error 
+ * occurred in the context of a sent request. 
+ *
+ * @package Application Utils
+ * @subpackage RequestHelper
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
 class RequestHelper_Exception extends BaseException
 {
+   /**
+    * @var RequestHelper_Response|NULL
+    */
+    protected $response = null;
+ 
+    public function setResponse(RequestHelper_Response $response)
+    {
+        $this->response = $response;
+    }
+    
+   /**
+    * Retrieves the related response instance, if available.
+    * 
+    * @return RequestHelper_Response|NULL
+    */
+    public function getResponse() : ?RequestHelper_Response
+    {
+        return $this->response;
+    }
 }
