d1d4106328365548832280a187e873e3a71fa9c0
Mistralys <s.mordziol@gmail.com>
2020-01-10 17:40:40 +0100

    Moved code into subclasses; Now using CURL.

--- src/RequestHelper.php
+++ src/RequestHelper.php
@@ -2,7 +2,7 @@
 /**
  * File containing the {@link RequestHelper} class.
  * @package Application Utils
- * @subpackage Request
+ * @subpackage RequestHelper
  * @see RequestHelper
  */
 
@@ -14,7 +14,7 @@ namespace AppUtils;
  * using file_get_contents with the according context parameters.
  *
  * @package Application Utils
- * @subpackage Request
+ * @subpackage RequestHelper
  * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
  */
 class RequestHelper
@@ -27,167 +27,291 @@ class RequestHelper
 
     const ERROR_REQUEST_FAILED = 17902;
     
+    const ERROR_CURL_INIT_FAILED = 17903;
+
+   /**
+    * @var string
+    */
     protected $eol = "\r\n";
 
+   /**
+    * @var string
+    */
     protected $mimeBoundary;
 
+   /**
+    * @var string
+    */
     protected $data = '';
 
+   /**
+    * @var string
+    */
     protected $destination;
 
-    /**
-     * Creates a new request helper to send POST data to the specified destination URL.
-     * @param string $destination
-     */
-    public function __construct($destination)
+   /**
+    * @var array
+    */
+    protected $headers = array();
+    
+   /**
+    * Whether to verify SSL certificates.
+    * @var bool
+    */
+    protected $verifySSL = true;
+    
+   /**
+    * @var RequestHelper_Boundaries
+    */
+    protected $boundaries;
+    
+   /**
+    * @var RequestHelper_Response|NULL
+    */
+    protected $response;
+
+   /**
+    * @var integer
+    */
+    protected $timeout = 30;
+    
+   /**
+    * Creates a new request helper to send POST data to the specified destination URL.
+    * @param string $destinationURL
+    */
+    public function __construct(string $destinationURL)
     {
-        $this->destination = $destination;
-        $this->mimeBoundary = '----------------super_boundary';
+        $this->destination = $destinationURL;
+        $this->mimeBoundary = md5('request-helper-boundary');
+        $this->boundaries = new RequestHelper_Boundaries($this);
+        
+        requireCURL();
+    }
+    
+    public function getMimeBoundary() : string
+    {
+        return $this->mimeBoundary;
+    }
+    
+    public function getEOL() : string
+    {
+        return $this->eol;
+    }
+    
+    public function setTimeout(int $seconds) : RequestHelper
+    {
+        $this->timeout = $seconds;
+        
+        return $this;
     }
 
-    protected $files = array();
-
-    /**
-     * Adds a file to be sent with the request.
-     *
-     * @param string $varName The variable name to send the file in
-     * @param string $fileName The name of the file as it should be received at the destination
-     * @param string $content The raw content of the file
-     * @param string $contentType The content type, use the constants to specify this
-     * @param string $encoding The encoding of the file, use the constants to specify this
-     */
-    public function addFile($varName, $fileName, $content, $contentType = self::FILETYPE_TEXT, $encoding = self::ENCODING_UTF8)
+   /**
+    * Adds a file to be sent with the request.
+    *
+    * @param string $varName The variable name to send the file in
+    * @param string $fileName The name of the file as it should be received at the destination
+    * @param string $content The raw content of the file
+    * @param string $contentType The content type, use the constants to specify this
+    * @param string $encoding The encoding of the file, use the constants to specify this
+    */
+    public function addFile(string $varName, string $fileName, string $content, string $contentType = self::FILETYPE_TEXT, string $encoding = self::ENCODING_UTF8) : RequestHelper
     {
-        $this->files[$varName] = array(
-            'fileName' => basename($fileName),
-            'content' => chunk_split(base64_encode($content)),
-            'contentType' => $contentType,
-            'encoding' => $encoding
-        );
+        $this->boundaries->addFile($varName, $fileName, $content, $contentType, $encoding);
+        
+        return $this;
+    }
+    
+   /**
+    * Adds arbitrary content.
+    * 
+    * @param string $varName The variable name to send the content in.
+    * @param string $content
+    * @param string $contentType
+    */
+    public function addContent(string $varName, string $content, string $contentType) : RequestHelper
+    {
+        $this->boundaries->addContent($varName, $content, $contentType);
+        
+        return $this;
     }
 
-    protected $variables = array();
-
     /**
-     * Adds a variable to be sent with the request.
+     * Adds a variable to be sent with the request. If it
+     * already exists, its value is overwritten.
      *
      * @param string $name
      * @param string $value
      */
-    public function addVariable($name, $value)
+    public function addVariable(string $name, string $value) : RequestHelper
     {
-        $this->variables[$name] = $value;
+        $this->boundaries->addVariable($name, $value);
+        
+        return $this;
     }
-
-    /**
-     * Sends the POST request to the destination, and returns
-     * the response text. Use the {@link getResponseHeader()}
-     * method to retrieve the response header if need be.
-     *
-     * The response text is stored internally, so after calling
-     * this method it may be retrieved at any moment using the
-     * {@link getResponse()} method.
-     *
-     * @return string
-     * @see getResponse()
-     * @see getResponseHeader()
-     */
-    public function send()
+    
+   /**
+    * Sets an HTTP header to include in the request.
+    * 
+    * @param string $name
+    * @param string $value
+    * @return RequestHelper
+    */
+    public function setHeader(string $name, string $value) : RequestHelper
     {
-        $contentLength = 0;
-        if (!empty($this->variables)) {
-            foreach ($this->variables as $name => $value) {
-                $this->data .= '--' . $this->mimeBoundary . $this->eol;
-                $this->data .= 'Content-Disposition: form-data; name="' . $name . '"' . $this->eol;
-                $this->data .= $this->eol;
-                $this->data .= $value . $this->eol;
+        $this->headers[$name] = $value;
+        
+        return $this;
+    }
+    
+   /**
+    * Disables SSL certificate checking.
+    * 
+    * @return RequestHelper
+    */
+    public function disableSSLChecks() : RequestHelper
+    {
+        $this->verifySSL = false;
+        return $this;
+    }
+   
+   /**
+    * @var integer
+    */
+    protected $contentLength = 0;
 
-                $contentLength += strlen($value);
-            }
+   /**
+    * Sends the POST request to the destination, and returns
+    * the response text.
+    *
+    * The response object is stored internally, so after calling
+    * this method it may be retrieved at any moment using the
+    * {@link getResponse()} method.
+    *
+    * @return string
+    * @see RequestHelper::getResponse()
+    * @throws RequestHelper_Exception
+    * 
+    * @see RequestHelper::ERROR_REQUEST_FAILED
+    */
+    public function send() : string
+    {
+        $this->data = $this->boundaries->render();
+
+        $info = parseURL($this->destination);
+        
+        $ch = $this->createCURL($info);
+        
+        $output = curl_exec($ch);
+
+        $info = curl_getinfo($ch);
+
+        $this->response = new RequestHelper_Response($this, $info);
+        
+        // CURL will complain about an empty response when the 
+        // server sends a 100-continue code. That should not be
+        // regarded as an error.
+        if($output === false && $this->response->getCode() !== 100)
+        {
+            $this->response->setError(
+                curl_errno($ch),
+                curl_error($ch)
+            );
         }
-
-        if (!empty($this->files)) {
-            foreach ($this->files as $varName => $def) {
-                $this->data .= '--' . $this->mimeBoundary . $this->eol;
-                $this->data .= 'Content-Disposition: form-data; name="' . $varName . '"; filename="' . $def['fileName'] . '"' . $this->eol;
-                $this->data .= 'Content-Type: ' . $def['contentType'] . '; charset=' . $def['encoding'];
-                $this->data .= $this->eol . $this->eol;
-                $this->data .= $def['content'] . $this->eol;
-
-                $contentLength += strlen($def['content']);
-            }
+        else
+        {
+            $this->response->setBody($output);
         }
-
-        $this->data .= "--" . $this->mimeBoundary . "--" . $this->eol . $this->eol; // finish with two eol's!!
-
-        //echo '<pre>'.print_r($this->data,true).'</pre>';
-
-        $params = array(
-            'http' => array(
-                'method' => 'POST',
-                'header' =>
-                    'Content-Length: ' . $contentLength . $this->eol .
-                    'Content-Type: multipart/form-data; charset=UTF-8; boundary=' . $this->mimeBoundary . $this->eol
-            ,
-                'content' => $this->data
-            )
-        );
-
-        global $http_response_header;
-        $ctx = stream_context_create($params);
-
-        $this->response = @file_get_contents($this->destination, null, $ctx);
-        if ($this->response === false) {
-            $errorMessage = error_get_last();
+        
+        curl_close($ch);
+        
+        return $this->response->getResponseBody();
+    }
+    
+    public function getBody() : string
+    {
+        return $this->data;
+    }
+    
+   /**
+    * Creates a new CURL resource configured according to the
+    * request's settings.
+    * 
+    * @param URLInfo $url
+    * @throws RequestHelper_Exception
+    * @return resource
+    */
+    protected function createCURL(URLInfo $url)
+    {
+        $ch = curl_init();
+        if($ch === false)
+        {
             throw new RequestHelper_Exception(
-                'Request failed.',
-                'Request error message: ' . $errorMessage['message'] . ' in ' . $errorMessage['file'] . ':' . $errorMessage['line'],
-                self::ERROR_REQUEST_FAILED
+                'Could not initialize a new cURL instance.',
+                'Calling curl_init returned false. Additional information is not available.',
+                self::ERROR_CURL_INIT_FAILED
             );
         }
 
-        $this->responseHeader = $http_response_header;
-
-        //echo '<pre>'.print_r($this->responseHeader,true).'</pre>';
-        //exit;
-
-        return $this->response;
+        $this->setHeader('Content-Length', $this->boundaries->getContentLength());
+        $this->setHeader('Content-Type', 'multipart/form-data; charset=UTF-8; boundary=' . $this->mimeBoundary);
+        
+        //curl_setopt($ch, CURLOPT_VERBOSE, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_URL, $url->getNormalizedWithoutAuth());
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->renderHeaders());
+        
+        if($this->verifySSL)
+        {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        }
+        
+        if($url->hasUsername())
+        {
+            curl_setopt($ch, CURLOPT_USERNAME, $url->getUsername());
+            curl_setopt($ch, CURLOPT_PASSWORD, $url->getPassword());
+        }
+        
+        return $ch;
     }
 
-    protected $response;
-
-    protected $responseHeader;
-
-    /**
-     * Retrieves the raw response header, in the form of an indexed
-     * array containing all response header lines, for example:
-     *
-     * Array
-     * (
-     *     [0] => HTTP/1.1 200 OK
-     *     [1] => Date: Thu, 31 Jan 2013 10:44:21 GMT
-     *     [2] => Server: Apache/2.2.22 (Win32) mod_ssl/2.2.22 OpenSSL/0.9.8o
-     *     [3] => X-Powered-By: PHP/5.3.14 ZendServer
-     *     [4] => Set-Cookie: ZDEDebuggerPresent=php,phtml,php3; path=/
-     *     [5] => Content-Disposition: attachment; filename=converted.csv
-     *     [6] => Pragma: no-cache
-     *     [7] => Connection: close
-     *     [8] => Content-Type: text/csv; charset=utf-8
-     * )
-     */
+    protected function renderHeaders() : array
+    {
+        $result = array();
+        
+        foreach($this->headers as $name => $value) {
+            $result[] = $name.': '.$value;
+        }
+        
+        return $result;
+    }
+    
+   /**
+    * Retrieves the raw response header, in the form of an indexed
+    * array containing all response header lines, for example:
+    */
     public function getResponseHeader()
     {
-        return $this->responseHeader;
+        return $this->response->getInfo();
     }
 
     /**
      * After calling the {@link send()} method, this may be used to
      * retrieve the response text from the POST request.
      *
-     * @return string
+     * @return RequestHelper_Response|NULL
      */
-    public function getResponse()
+    public function getResponse() : ?RequestHelper_Response
     {
         return $this->response;
+    }
+    
+    public function getHeaders()
+    {
+        return $this->headers;
     }
 }
