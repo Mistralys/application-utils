2508f8f4214fecb8a26871d88c8ea3a76937c6ea
Mistralys <s.mordziol@gmail.com>
2020-03-01 13:49:15 +0100

    Added required parameter.

--- src/URLInfo/Normalizer.php
+++ src/URLInfo/Normalizer.php
@@ -119,7 +119,7 @@ class URLInfo_Normalizer
             return $normalized;
         }
         
-        return $normalized . '?'.http_build_query($params, null, '&', PHP_QUERY_RFC3986);
+        return $normalized . '?'.http_build_query($params, '', '&', PHP_QUERY_RFC3986);
     }
     
     protected function renderFragment(string $normalized) : string
