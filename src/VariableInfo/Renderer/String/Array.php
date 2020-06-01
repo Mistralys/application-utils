b71c2cdb310b4be62ae98cbc49fc008451af9285
Mistralys <s.mordziol@gmail.com>
2020-06-01 13:19:40 +0200

    Fixed the indendation of the arrays.

--- src/VariableInfo/Renderer/String/Array.php
+++ src/VariableInfo/Renderer/String/Array.php
@@ -8,13 +8,27 @@ class VariableInfo_Renderer_String_Array extends VariableInfo_Renderer_String
 {
     protected function _render()
     {
-        $result = array();
-        
-        foreach($this->value as $key => $val)
-        {
-            $result[$key] = parseVariable($val)->toString();
-        }
+        $result = $this->traverseArray($this->value);
         
         return print_r($result, true);
+    }
+    
+    protected function traverseArray(array $array, int $level=0) : array
+    {
+        $result = array();
+        
+        foreach($array as $key => $val)
+        {
+            if(is_array($val))
+            {
+                $result[$key] = $this->traverseArray($val, ($level+1));
+            }
+            else
+            {
+                $result[$key] = parseVariable($val)->toString();
+            }
+        }
+        
+        return $result;
     }
 }
