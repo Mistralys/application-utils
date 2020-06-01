b691b7cfe98994ad2fee1e1487a77ed189d5ac5b
Mistralys <s.mordziol@gmail.com>
2020-06-01 13:20:06 +0200

    Added valBool(), valBoolTrue() and valBoolFalse().

--- src/functions.php
+++ src/functions.php
@@ -127,6 +127,43 @@ function requireCURL() : void
 }
 
 /**
+ * Creates a boolean value.
+ * 
+ * @param bool $initial The initial boolean value to use.
+ * @return Value_Bool
+ */
+function valBool(bool $initial=false) : Value_Bool
+{
+    return new Value_Bool($initial);
+}
+
+/**
+ * Creates a sticky true-based boolean value: starts out
+ * as false, and if it is set to true, cannot be set to
+ * false again afterwards.
+ * 
+ * @param bool $initial
+ * @return Value_Bool_True
+ */
+function valBoolTrue(bool $initial=false) : Value_Bool_True
+{
+    return new Value_Bool_True($initial);
+}
+
+/**
+ * Creates a sticky false-based boolean value: starts out
+ * as true, and if it is set to false, cannot be set to
+ * true again afterwards.
+ * 
+ * @param bool $initial
+ * @return Value_Bool_False
+ */
+function valBoolFalse(bool $initial=true) : Value_Bool_False
+{
+    return new Value_Bool_False($initial);
+}
+
+/**
  * Initializes the utilities: this is called automatically
  * because this file is included in the files list in the
  * composer.json, guaranteeing it is always loaded.
