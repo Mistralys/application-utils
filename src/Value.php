eba2ce9c48b8ef398fc5ee6207ca46173e6d8f86
Mistralys <s.mordziol@gmail.com>
2020-06-01 13:18:33 +0200

    Added the Value base class used for OO variable types.

---
+++ src/Value.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * File containing the {@see AppUtils\Value} class.
+ *
+ * @package Application Utils
+ * @subpackage Values
+ * @see AppUtils\Value
+ */
+
+declare(strict_types=1);
+
+namespace AppUtils;
+
+/**
+ * Abstract base class for value type representations.
+ *
+ * @package Application Utils
+ * @subpackage Values
+ */
+abstract class Value
+{
+    
+}
