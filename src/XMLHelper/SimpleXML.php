207b67a18988ccf9a3a1c24bdd75fef80f3880c6
Mistralys <s.mordziol@gmail.com>
2020-06-02 09:07:44 +0200

    Cleanup & improvements.
    - Using strict types.
    - Added return types.
    - toArray() now uses the XMLHelper converter.
    - Added toJSON().
    - Added getConverter().
    - Added an exception when accessing infos before loading a file or string.
    - Added inline docs.

--- src/XMLHelper/SimpleXML.php
+++ src/XMLHelper/SimpleXML.php
@@ -1,15 +1,37 @@
 <?php
+/**
+ * File containing the {@see XMLHelper_SimpleXML} class.
+ * 
+ * @package AppUtils
+ * @subpackage XMLHelper
+ * @see XMLHelper_SimpleXML
+ */
+
+declare(strict_types=1);
 
 namespace AppUtils;
 
+/**
+ * Utility class to create SimpleXML elements, with
+ * easier error handling. 
+ * 
+ * @package AppUtils
+ * @subpackage XMLHelper
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
 class XMLHelper_SimpleXML
 {
-    /**
-     * @var \SimpleXMLElement
-     */
-    protected $xml;
+    const ERROR_NOT_LOADED_YET = 56501;
     
-    protected $errors = array();
+   /**
+    * @var \SimpleXMLElement|NULL
+    */
+    private $element = null;
+    
+   /**
+    * @var XMLHelper_SimpleXML_Error[]
+    */
+    private $errors = array();
     
    /**
     * Creates a simplexml instance from an XML string.
@@ -17,9 +39,9 @@ class XMLHelper_SimpleXML
     * NOTE: returns false in case of a fatal error.
     *
     * @param string $string
-    * @return \SimpleXMLElement|bool
+    * @return \SimpleXMLElement|NULL
     */
-    public function loadString($string)
+    public function loadString(string $string) : ?\SimpleXMLElement
     {
         return $this->load('string', $string);
     }
@@ -30,53 +52,86 @@ class XMLHelper_SimpleXML
     * NOTE: returns false in case of a fatal error.
     * 
     * @param string $file
-    * @return \SimpleXMLElement|bool
+    * @return \SimpleXMLElement|NULL
     */
-    public function loadFile($file)
+    public function loadFile(string $file) : ?\SimpleXMLElement
     {
         return $this->load('file', $file);
     }
     
-    protected function load($mode, $subject)
+    private function load(string $mode, string $subject) : ?\SimpleXMLElement
     { 
         $this->errors = array();
-        
-        $function = 'simplexml_load_'.$mode;
         
         // to be able to fetch errors, we have to 
         // enable the internal errors.
         $use_errors = libxml_use_internal_errors(true);
         
-        $this->xml = $function($subject);
+        $this->element = $this->createInstance($mode, $subject);
         
-        // add any errors that were triggered, using the
-        // error wrappers.
-        $errors = libxml_get_errors();
-        foreach($errors as $error) {
-            $this->errors[] = new XMLHelper_SimpleXML_Error($this, $error);
-        }
-        
-        libxml_clear_errors();
+        $this->detectErrors();
         
         // restore the previous setting just in case
         libxml_use_internal_errors($use_errors);
         
-        return $this->xml;
+        return $this->element;
     }
     
-    public function toArray()
+    private function detectErrors() : void
     {
-        $json = json_encode($this->xml);
-        return json_decode($json, true);
+        // add any errors that were triggered, using the
+        // error wrappers.
+        $errors = libxml_get_errors();
+        
+        foreach($errors as $error) 
+        {
+            $this->errors[] = new XMLHelper_SimpleXML_Error($this, $error);
+        }
+        
+        libxml_clear_errors();
     }
     
-    public function dispose()
+    private function createInstance(string $mode, string $subject) : ?\SimpleXMLElement
     {
-        $this->xml = null;
+        $function = 'simplexml_load_'.$mode;
+        
+        $xml = $function($subject);
+        
+        if($xml instanceof \SimpleXMLElement)
+        {
+            return $xml;
+        }
+        
+        return null;
+    }
+    
+    public function getConverter() : XMLHelper_Converter
+    {
+        if($this->element instanceof \SimpleXMLElement)
+        {
+            return XMLHelper::convertElement($this->element);
+        }
+        
+        throw $this->createNotLoadedException(); 
+    }
+     
+    public function toArray() : array
+    {
+        return $this->getConverter()->toArray();
+    }
+    
+    public function toJSON() : string
+    {
+        return $this->getConverter()->toJSON();
+    }
+    
+    public function dispose() : void
+    {
+        $this->element = null;
         $this->errors = array();
     }
     
-    public function hasErrors()
+    public function hasErrors() : bool
     {
         return !empty($this->errors);
     }
@@ -88,5 +143,14 @@ class XMLHelper_SimpleXML
     public function getErrorMessages()
     {
         return $this->errors;
+    }
+    
+    private function createNotLoadedException() : XMLHelper_Exception
+    {
+        return new XMLHelper_Exception(
+            'No SimpleXML element loaded.',
+            'No element has been loaded yet. The loadFile() or loadString() method must be called first.',
+            self::ERROR_NOT_LOADED_YET
+        );
     }
 }
