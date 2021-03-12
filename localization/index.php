eeefab2420c43fda70e2066381b818efb4219c23
Mistralys <s.mordziol@gmail.com>
2021-03-12 11:00:07 +0100

    Added the source folder to search in the correct location.

--- localization/index.php
+++ localization/index.php
@@ -6,9 +6,12 @@
  * @subpackage Localization
  * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
  */
-    
+
+    declare(strict_types=1);
+
+    use AppLocalize\Localization;
+
     $root = __DIR__;
-    
     $autoload = $root.'/vendor/autoload.php';
     
     // we need the autoloader to be present
@@ -23,12 +26,25 @@
     require_once $autoload;
     
     // add the locales we wish to manage (en_UK is always present)
-    \AppLocalize\Localization::addAppLocale('de_DE');
-    \AppLocalize\Localization::addAppLocale('fr_FR');
+    Localization::addAppLocale('de_DE');
+    Localization::addAppLocale('fr_FR');
     
     // has to be called last after all sources and locales have been configured
-    \AppLocalize\Localization::configure($root.'/storage.json', '');
-    
+    Localization::configure($root.'/storage.json', '');
+
+    $installFolder = realpath(__DIR__.'/../');
+
+    // Register the classes as a localization source,
+    // so they can be found, and use the bundled localization
+    // files.
+    Localization::addSourceFolder(
+        'application-utils-localization',
+        'AppUtils Localization',
+        'Composer Packages',
+        $installFolder.'/localization',
+        $installFolder.'/src'
+    );
+
     // create the editor UI and start it
-    $editor = \AppLocalize\Localization::createEditor();
+    $editor = Localization::createEditor();
     $editor->display();
