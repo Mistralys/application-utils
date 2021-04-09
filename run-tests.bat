6ea7dfff6d15b074bb42ead67a13db4aafa594ff
Mistralys <s.mordziol@gmail.com>
2021-04-09 17:11:11 +0200

    Passing parameters through to phpunit.

--- run-tests.bat
+++ run-tests.bat
@@ -2,7 +2,7 @@
 
 cls 
 
-call php -d upload_max_filesize=6M -d post_max_size=6M vendor/phpunit/phpunit/phpunit
+call php -d upload_max_filesize=6M -d post_max_size=6M vendor/phpunit/phpunit/phpunit %*
 
 echo.
 echo.
