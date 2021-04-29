9adbf5cc73af575d3cd85c1bff9b7f5b5e6b6a40
Mistralys <s.mordziol@gmail.com>
2021-04-29 15:04:29 +0200

    Added handling of more callable types.

--- src/VariableInfo/Renderer/String/Callable.php
+++ src/VariableInfo/Renderer/String/Callable.php
@@ -4,23 +4,47 @@ declare(strict_types=1);
 
 namespace AppUtils;
 
+use Closure;
+
 class VariableInfo_Renderer_String_Callable extends VariableInfo_Renderer_String
 {
     protected function _render()
     {
         $string = '';
-        
-        if(is_string($this->value[0])) 
+
+        // Simple function call
+        if(is_string($this->value))
         {
-            $string .= $this->value[0].'::';
-        } 
-        else 
-        {
-            $string .= get_class($this->value[0]).'->';
+            return $this->value.'()';
         }
-        
+
+        if(is_array($this->value)) {
+            return $this->renderArray();
+        }
+
+        if($this->value instanceof NamedClosure) {
+            return 'Closure['.$this->value->getOrigin().']';
+        }
+
+        if($this->value instanceof Closure) {
+            return 'Closure';
+        }
+
+        return $string;
+    }
+
+    private function renderArray() : string
+    {
+        $string = '';
+
+        if (is_string($this->value[0])) {
+            $string .= $this->value[0] . '::';
+        } else {
+            $string .= get_class($this->value[0]) . '->';
+        }
+
         $string .= $this->value[1].'()';
-        
+
         return $string;
     }
 }
