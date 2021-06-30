c1f8725ac8d5e3e426f065b378d834a2b8cf3ce5
Mistralys <s.mordziol@gmail.com>
2021-06-30 14:08:10 +0200

    Added the basic stringable interface for objects with __toString().

---
+++ src/Interface/Stringable.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppUtils;
+
+interface Interface_Stringable
+{
+    /**
+     * Converts the object to a string.
+     *
+     * @return string
+     */
+    public function __toString();
+}
