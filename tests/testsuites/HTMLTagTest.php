c1c9a34a6c1cb86ae795faf9715c0b9a3aa419e3
Mistralys <s.mordziol@gmail.com>
2022-01-07 14:30:25 +0100

    Added tests.

--- tests/testsuites/HTMLTagTest.php
+++ tests/testsuites/HTMLTagTest.php
@@ -3,17 +3,23 @@
 declare(strict_types=1);
 
 use AppUtils\HTMLTag;
+use AppUtils\HTMLTag\CannedTags;
 use PHPUnit\Framework\TestCase;
 
 final class HTMLTagTest extends TestCase
 {
+    protected function setUp() : void
+    {
+        HTMLTag::getGlobalOptions()->setSelfCloseStyle(HTMLTag::SELF_CLOSE_STYLE_NONE);
+    }
+
     public function test_contentAndAttributes() : void
     {
         $this->assertEquals(
             '<a href="https://testdomain.test">label</a>',
             HTMLTag::create('a')
-                ->attr('href', 'https://testdomain.test')
-                ->content('label')
+                ->href('https://testdomain.test')
+                ->setContent('label')
                 ->render()
         );
     }
@@ -32,7 +38,7 @@ final class HTMLTagTest extends TestCase
         $this->assertEquals(
             '<code>content</code>',
             (string)HTMLTag::create('code')
-                ->append('content')
+                ->addText('content')
         );
     }
 
@@ -60,9 +66,51 @@ final class HTMLTagTest extends TestCase
     public function test_selfClosing() : void
     {
         $this->assertEquals(
+            '<br>',
+            (string)HTMLTag::create('br')
+                ->setSelfClosing()
+        );
+
+        HTMLTag::getGlobalOptions()->setSelfCloseSlash();
+
+        $this->assertEquals(
             '<br/>',
             (string)HTMLTag::create('br')
                 ->setSelfClosing()
         );
+    }
+
+    public function test_cannedTags() : void
+    {
+        $tests = array(
+            array(
+                'tag' => CannedTags::anchor('/path/to/page', 'label'),
+                'expected' => '<a href="/path/to/page">label</a>'
+            ),
+            array(
+                'tag' => CannedTags::anchor('/path/to/page'),
+                'expected' => '<a href="/path/to/page"></a>'
+            ),
+            array(
+                'tag' => CannedTags::br(),
+                'expected' => '<br>'
+            ),
+            array(
+                'tag' => CannedTags::div('content'),
+                'expected' => '<div>content</div>'
+            ),
+            array(
+                'tag' => CannedTags::p('Text'),
+                'expected' => '<p>Text</p>'
+            )
+        );
+
+        foreach($tests as $test)
+        {
+            $this->assertEquals(
+                (string)$test['tag'],
+                $test['expected']
+            );
+        }
     }
 }
