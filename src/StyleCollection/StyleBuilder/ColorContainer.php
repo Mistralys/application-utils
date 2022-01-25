b3f2531e26ffe7c850262f9d9f8ba774a1372b8f
Mistralys <s.mordziol@gmail.com>
2022-01-25 09:38:33 +0100

    Added `hexString()` and `rgbaValues()`.

--- src/StyleCollection/StyleBuilder/ColorContainer.php
+++ src/StyleCollection/StyleBuilder/ColorContainer.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace AppUtils\StyleCollection\StyleBuilder;
 
 use AppUtils\RGBAColor;
+use AppUtils\RGBAColor\ColorException;
+use AppUtils\RGBAColor\ColorFactory;
 use AppUtils\StyleCollection\StyleBuilder;
 
 abstract class ColorContainer extends StyleContainer
 {
+    public function rgbaValues(int $red, int $green, int $blue, float $opacity=1) : StyleBuilder
+    {
+        return $this->rgba(ColorFactory::createCSS($red, $green, $blue, $opacity));
+    }
+
     public function rgba(RGBAColor $color, bool $important=false) : StyleBuilder
     {
         return $this->style($color->toCSS(), $important);
@@ -16,6 +23,19 @@ abstract class ColorContainer extends StyleContainer
 
     public function hex(RGBAColor $color, bool $important=false) : StyleBuilder
     {
-        return $this->style($color->toHEX(), $important);
+        return $this->style('#'.$color->toHEX(), $important);
+    }
+
+    /**
+     * Uses a HEX color value string.
+     *
+     * @param string $hex The color, e.g. "fff", "FAFAFA", or with alpha channel "CCCCCCAA"
+     * @param bool $important
+     * @return StyleBuilder
+     * @throws ColorException
+     */
+    public function hexString(string $hex, bool $important=false) : StyleBuilder
+    {
+        return $this->hex(ColorFactory::createFromHEX($hex), $important);
     }
 }
