04ff5c2d43dc2eddc77500d78da1e1b7a76caa9c
Mistralys <s.mordziol@gmail.com>
2022-06-08 12:30:29 +0200

    Extended tests to pinpoint Travis issues.

--- tests/testsuites/FileHelperTests/ResolvePathTypeTest.php
+++ tests/testsuites/FileHelperTests/ResolvePathTypeTest.php
@@ -4,34 +4,89 @@ declare(strict_types=1);
 
 namespace testsuites\FileHelperTests;
 
+use AppUtils\ConvertHelper;
 use AppUtils\FileHelper\AbstractPathInfo;
 use DirectoryIterator;
 use TestClasses\FileHelperTestCase;
 
 final class ResolvePathTypeTest extends FileHelperTestCase
 {
-    public function test_resolveTypes() : void
+    /**
+     * Sanity check to ensure the prerequisites are
+     * everything as expected. This was added because
+     * during the Travis tests, some folders were
+     * detected as files.
+     */
+    public function test_controlIteratorBehavior() : void
     {
-        $iterator = new DirectoryIterator(__DIR__);
+        $folderPath = __DIR__.'/../../assets/FileHelper/FolderTree';
+
+        $iterator = new DirectoryIterator($folderPath);
+
         $this->assertTrue(
             $iterator->isDir(),
             sprintf(
                 'Iterator is not a folder: '.PHP_EOL.
                 '[%s]'.PHP_EOL.
-                'Created from __DIR__ variable: '.PHP_EOL.
+                'Folder is readable: '.PHP_EOL.
                 '[%s]',
                 $iterator->getPath(),
-                __DIR__
+                ConvertHelper::boolStrict2string(is_readable($folderPath))
             )
         );
+    }
 
-        $stringFolder = AbstractPathInfo::resolveType(__DIR__);
-        $stringFile = AbstractPathInfo::resolveType(__FILE__);
-        $iteratorFolder = AbstractPathInfo::resolveType($iterator);
+    public function test_resolveType_FolderString() : void
+    {
+        $path = __DIR__;
+        $this->assertTrue(is_readable($path));
+        $this->assertTrue(is_dir($path));
 
-        $this->assertTrue($stringFolder->isFolder(), 'Should be a folder: ['.$stringFolder.']');
-        $this->assertTrue($iteratorFolder->isFolder(), 'Should be a folder: ['.$iteratorFolder.']');
-        $this->assertTrue($stringFile->isFile(), 'Should be a file: ['.$stringFile.']');
+        $stringFolder = AbstractPathInfo::resolveType($path);
+
+        $this->assertTrue(
+            $stringFolder->isFolder(),
+            'Must be detected as a folder: ['.$stringFolder.']'
+        );
+    }
+
+    public function test_resolveType_FolderIterator() : void
+    {
+        $path = __DIR__;
+        $this->assertTrue(is_readable($path));
+        $this->assertTrue(is_dir($path));
+
+        $iteratorFolder = AbstractPathInfo::resolveType(new DirectoryIterator($path));
+
+        $this->assertTrue(
+            $iteratorFolder->isFolder(),
+            'Must be detected as a folder: ['.$iteratorFolder.']'
+        );
+    }
+
+    /**
+     * When passing an existing path info instance
+     * to {@see AbstractPathInfo::resolveType()}, this
+     * must not be modified and directly passed through.
+     */
+    public function test_instancePassThrough() : void
+    {
+        $stringFolder = AbstractPathInfo::resolveType('/path/to/folder');
+
         $this->assertSame($stringFolder, AbstractPathInfo::resolveType($stringFolder));
+    }
+
+    public function test_resolveType_File() : void
+    {
+        $path = __FILE__;
+        $this->assertTrue(is_readable($path));
+        $this->assertTrue(is_file($path));
+
+        $stringFile = AbstractPathInfo::resolveType($path);
+
+        $this->assertTrue(
+            $stringFile->isFile(),
+            'Should be a file: ['.$stringFile.']'
+        );
     }
 }
