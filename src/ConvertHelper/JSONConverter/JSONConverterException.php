238a6bed93dda8bffe6a0b2072948e9f7f51fab0
Mistralys <s.mordziol@gmail.com>
2022-09-14 12:34:38 +0200

    Added an exception.

---
+++ src/ConvertHelper/JSONConverter/JSONConverterException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Application Utils
+ * @subpackage ConvertHelper
+ * @see \AppUtils\ConvertHelper\JSONConverter\JSONConverterException
+ */
+
+declare(strict_types=1);
+
+namespace AppUtils\ConvertHelper\JSONConverter;
+
+use AppUtils\ConvertHelper_Exception;
+
+/**
+ * Specialized exception for the JSON converter.
+ *
+ * @package Application Utils
+ * @subpackage ConvertHelper
+ * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
+ */
+class JSONConverterException extends ConvertHelper_Exception
+{
+
+}
