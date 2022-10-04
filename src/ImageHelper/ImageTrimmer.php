a06701a776f0f75ced973153c5c4a67d963c8cd1
Mistralys <s.mordziol@gmail.com>
2022-10-04 12:12:12 +0200

    Restored the old way of trinming.
    To fix issues with borked colors.

--- src/ImageHelper/ImageTrimmer.php
+++ src/ImageHelper/ImageTrimmer.php
@@ -64,7 +64,7 @@ class ImageTrimmer
     /**
      * @return resource|GdImage|null
      */
-    public function trim()
+    public function trim_new()
     {
         $img = imagecropauto(
             $this->sourceImage,
@@ -84,5 +84,87 @@ class ImageTrimmer
         }
 
         return null;
+    }
+
+    public function trim()
+    {
+        // Get the image width and height.
+        $imw = imagesx($this->sourceImage);
+        $imh = imagesy($this->sourceImage);
+
+        // Set the X variables.
+        $xmin = $imw;
+        $xmax = 0;
+        $ymin = null;
+        $ymax = null;
+
+        // Start scanning for the edges.
+        for ($iy=0; $iy<$imh; $iy++)
+        {
+            $first = true;
+
+            for ($ix=0; $ix<$imw; $ix++)
+            {
+                $ndx = imagecolorat($this->sourceImage, $ix, $iy);
+                $colors = $this->helper->getIndexedColors($this->sourceImage, $ndx);
+
+                if(!$colors->matchesAlpha($this->trimColor))
+                {
+                    if ($xmin > $ix) { $xmin = $ix; }
+                    if ($xmax < $ix) { $xmax = $ix; }
+                    if (!isset($ymin)) { $ymin = $iy; }
+
+                    $ymax = $iy;
+
+                    if($first)
+                    {
+                        $ix = $xmax;
+                        $first = false;
+                    }
+                }
+            }
+        }
+
+        // no trimming border found
+        if($ymax === null) {
+            return $this->sourceImage;
+        }
+
+        // The new width and height of the image.
+        $imw = 1+$xmax-$xmin; // Image width in pixels
+        $imh = 1+$ymax-$ymin; // Image height in pixels
+
+        // Make another image to place the trimmed version in.
+        $im2 = $this->helper->createNewImage($imw, $imh);
+
+        if($this->trimColor->hasTransparency())
+        {
+            $bg2 = imagecolorallocatealpha(
+                $im2,
+                $this->trimColor->getRed()->get8Bit(),
+                $this->trimColor->getGreen()->get8Bit(),
+                $this->trimColor->getBlue()->get8Bit(),
+                $this->trimColor->getAlpha()->get7Bit()
+            );
+
+            imagecolortransparent($im2, $bg2);
+        }
+        else
+        {
+            $bg2 = imagecolorallocate(
+                $im2,
+                $this->trimColor->getRed()->get8Bit(),
+                $this->trimColor->getGreen()->get8Bit(),
+                $this->trimColor->getBlue()->get8Bit()
+            );
+        }
+
+        // Make the background of the new image the same as the background of the old one.
+        imagefill($im2, 0, 0, $bg2);
+
+        // Copy it over to the new image.
+        imagecopy($im2, $this->sourceImage, 0, 0, $xmin, $ymin, $imw, $imh);
+
+        return $im2;
     }
 }
