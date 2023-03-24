5cc092a6dfe0605d56c27c3cf955e6746ed38f9f
Mistralys <s.mordziol@gmail.com>
2023-03-24 10:46:58 +0100

    Added tests.

--- tests/testsuites/HSVColorTests/CoreTest.php
+++ tests/testsuites/HSVColorTests/CoreTest.php
@@ -31,4 +31,48 @@ final class CoreTest extends TestCase
 
         $this->assertSame(0.6, $rgb->getAlpha()->getAlpha());
     }
+
+    public function test_isColorDark() : void
+    {
+        $tests = array(
+            array(
+                'label' => 'Light color',
+                'hex' => 'ccc',
+                'dark' => false
+            ),
+            array(
+                'label' => 'Exactly 50% luminosity',
+                'hex' => '808080',
+                'dark' => false
+            ),
+            array(
+                'label' => 'Slightly below 50% luminosity',
+                'hex' => '7f7f7f',
+                'dark' => true
+            ),
+            array(
+                'label' => 'Black',
+                'hex' => '000',
+                'dark' => true
+            ),
+            array(
+                'label' => 'White',
+                'hex' => 'fff',
+                'dark' => false
+            )
+        );
+
+        foreach($tests as $test)
+        {
+            $color = ColorFactory::createFromHEX($test['hex']);
+            $brightness = $color->getBrightness();
+
+            $this->assertSame(
+                $brightness->getPercent() < 50,
+                $test['dark'],
+                'Test label: '.$test['label'].PHP_EOL.
+                'Brightness: '.$brightness->getPercent()
+            );
+        }
+    }
 }
