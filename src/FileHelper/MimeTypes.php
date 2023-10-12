6e87ee1beb20ee64c1d8c471b40262d4624a62bb
Mistralys <s.mordziol@gmail.com>
2023-10-12 08:30:39 +0200

    Added methods and mime constants.

--- src/FileHelper/MimeTypes.php
+++ src/FileHelper/MimeTypes.php
@@ -11,6 +11,8 @@ declare(strict_types=1);
 
 namespace AppUtils;
 
+use AppUtils\FileHelper\MimeTypesEnum;
+
 /**
  * Collection of file mime types by extension.
  *
@@ -20,13 +22,16 @@ namespace AppUtils;
  */
 class FileHelper_MimeTypes
 {
+
     /**
      * List of extensions that can typically be opened in a
      * browser natively, without needing separate software.
      *
      * @var string[]
      */
-    protected static $browserExtensions = array(
+    protected static array $browserExtensions = self::DEFAULT_BROWSER_EXTENSIONS;
+
+    public const DEFAULT_BROWSER_EXTENSIONS = array(
         // Text based files
         'html',
         'htm',
@@ -76,7 +81,9 @@ class FileHelper_MimeTypes
      * Table with file extension => mime type mappings
      * @var array<string|int,string>
      */
-    protected static $mimeTypes = array(
+    protected static array $mimeTypes = self::DEFAULT_MIME_TYPES;
+
+    public const DEFAULT_MIME_TYPES = array(
         '323' => 'text/h323',
         'acx' => 'application/internet-property-stream',
         'ai' => 'application/postscript',
@@ -86,18 +93,18 @@ class FileHelper_MimeTypes
         'asf' => 'video/x-ms-asf',
         'asr' => 'video/x-ms-asf',
         'asx' => 'video/x-ms-asf',
-        'au' => 'audio/basic',
-        'avi' => 'video/x-msvideo',
+        'au' => MimeTypesEnum::MIME_AUDIO_BASIC,
+        'avi' => MimeTypesEnum::MIME_VIDEO_AVI,
         'axs' => 'application/olescript',
-        'bas' => 'text/plain',
+        'bas' => MimeTypesEnum::MIME_TEXT_PLAIN,
         'bcpio' => 'application/x-bcpio',
-        'bin' => 'application/octet-stream',
-        'bmp' => 'image/bmp',
-        'c' => 'text/plain',
+        'bin' => MimeTypesEnum::MIME_APPLICATION_OCTET_STREAM,
+        'bmp' => MimeTypesEnum::MIME_IMAGE_BMP,
+        'c' => MimeTypesEnum::MIME_TEXT_PLAIN,
         'cat' => 'application/vnd.ms-pkiseccat',
         'cdf' => 'application/x-cdf',
         'cer' => 'application/x-x509-ca-cert',
-        'class' => 'application/octet-stream',
+        'class' => MimeTypesEnum::MIME_APPLICATION_OCTET_STREAM,
         'clp' => 'application/x-msclip',
         'cmx' => 'image/x-cmx',
         'cod' => 'mage/cis-cod',
@@ -106,79 +113,79 @@ class FileHelper_MimeTypes
         'crl' => 'application/pkix-crl',
         'crt' => 'application/x-x509-ca-cert',
         'csh' => 'application/x-csh',
-        'css' => 'text/css',
-        'csv' => 'text/csv',
+        'css' => MimeTypesEnum::MIME_TEXT_CSS,
+        'csv' => MimeTypesEnum::MIME_TEXT_CSV,
         'dcr' => 'application/x-director',
         'dds' => 'image/vnd-ms.dds',
         'der' => 'application/x-x509-ca-cert',
         'dir' => 'application/x-director',
         'dll' => 'application/x-msdownload',
-        'dms' => 'application/octet-stream',
-        'doc' => 'application/msword',
+        'dms' => MimeTypesEnum::MIME_APPLICATION_OCTET_STREAM,
+        'doc' => MimeTypesEnum::MIME_APPLICATION_MSWORD,
         'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        'dot' => 'application/msword',
+        'dot' => MimeTypesEnum::MIME_APPLICATION_MSWORD,
         'dvi' => 'application/x-dvi',
         'dxr' => 'application/x-director',
         'eps' => 'application/postscript',
         'etx' => 'text/x-setext',
         'evy' => 'application/envoy',
-        'exe' => 'application/octet-stream',
+        'exe' => MimeTypesEnum::MIME_APPLICATION_OCTET_STREAM,
         'fif' => 'application/fractals',
         'flr' => 'x-world/x-vrml',
-        'gif' => 'image/gif',
-        'gtar' => 'application/x-gtar',
-        'gz' => 'application/x-gzip',
-        'h' => 'text/plain',
+        'gif' => MimeTypesEnum::MIME_IMAGE_GIF,
+        'gtar' => MimeTypesEnum::MIME_ARCHIVE_GTAR,
+        'gz' => MimeTypesEnum::MIME_ARCHIVE_GZIP,
+        'h' => MimeTypesEnum::MIME_TEXT_PLAIN,
         'hdf' => 'application/x-hdf',
         'hlp' => 'application/winhlp',
         'hqx' => 'application/mac-binhex40',
         'hta' => 'application/hta',
-        'htc' => 'text/x-component',
-        'htm' => 'text/html',
-        'html' => 'text/html',
+        'htc' => MimeTypesEnum::MIME_TEXT_COMPONENT,
+        'htm' => MimeTypesEnum::MIME_TEXT_HTML,
+        'html' => MimeTypesEnum::MIME_TEXT_HTML,
         'htt' => 'text/webviewhtml',
-        'ico' => 'image/x-icon',
-        'ief' => 'image/ief',
+        'ico' => MimeTypesEnum::MIME_IMAGE_ICON,
+        'ief' => MimeTypesEnum::MIME_IMAGE_IEF,
         'iii' => 'application/x-iphone',
         'ins' => 'application/x-internet-signup',
         'isp' => 'application/x-internet-signup',
         'jfif' => 'image/pipeg',
-        'jpe' => 'image/jpeg',
-        'jpeg' => 'image/jpeg',
-        'jpg' => 'image/jpeg',
-        'js' => 'application/x-javascript',
-        'json' => 'application/json',
+        'jpe' => MimeTypesEnum::MIME_IMAGE_JPEG,
+        'jpeg' => MimeTypesEnum::MIME_IMAGE_JPEG,
+        'jpg' => MimeTypesEnum::MIME_IMAGE_JPEG,
+        'js' => MimeTypesEnum::MIME_TEXT_JAVASCRIPT,
+        'json' => MimeTypesEnum::MIME_TEXT_JSON,
         'latex' => 'application/x-latex',
-        'lha' => 'application/octet-stream',
-        'lsf' => 'video/x-la-asf',
-        'lsx' => 'video/x-la-asf',
-        'lzh' => 'application/octet-stream',
+        'lha' => MimeTypesEnum::MIME_APPLICATION_OCTET_STREAM,
+        'lsf' => MimeTypesEnum::MIME_VIDEO_ASF,
+        'lsx' => MimeTypesEnum::MIME_VIDEO_ASF,
+        'lzh' => MimeTypesEnum::MIME_APPLICATION_OCTET_STREAM,
         'm13' => 'application/x-msmediaview',
         'm14' => 'application/x-msmediaview',
-        'm3u' => 'audio/x-mpegurl',
+        'm3u' => MimeTypesEnum::MIME_AUDIO_MPEGURL,
         'man' => 'application/x-troff-man',
         'mdb' => 'application/x-msaccess',
         'me' => 'application/x-troff-me',
-        'mht' => 'message/rfc822',
-        'mhtml' => 'message/rfc822',
-        'mid' => 'audio/mid',
+        'mht' => MimeTypesEnum::MIME_TEXT_RFC822,
+        'mhtml' => MimeTypesEnum::MIME_TEXT_RFC822,
+        'mid' => MimeTypesEnum::MIME_AUDIO_MID,
         'mny' => 'application/x-msmoney',
-        'mov' => 'video/quicktime',
+        'mov' => MimeTypesEnum::MIME_VIDEO_QUICKTIME,
         'movie' => 'video/x-sgi-movie',
-        'mp2' => 'video/mpeg',
-        'mp3' => 'audio/mpeg',
-        'mp4' => 'video/mp4',
-        'mpa' => 'video/mpeg',
-        'mpe' => 'video/mpeg',
-        'mpeg' => 'video/mpeg',
-        'mpg' => 'video/mpeg',
+        'mp2' => MimeTypesEnum::MIME_VIDEO_MPEG,
+        'mp3' => MimeTypesEnum::MIME_AUDIO_MPEG,
+        'mp4' => MimeTypesEnum::MIME_VIDEO_MP4,
+        'mpa' => MimeTypesEnum::MIME_VIDEO_MPEG,
+        'mpe' => MimeTypesEnum::MIME_VIDEO_MPEG,
+        'mpeg' => MimeTypesEnum::MIME_VIDEO_MPEG,
+        'mpg' => MimeTypesEnum::MIME_VIDEO_MPEG,
         'mpp' => 'application/vnd.ms-project',
-        'mpv2' => 'video/mpeg',
+        'mpv2' => MimeTypesEnum::MIME_VIDEO_MPEG,
         'ms' => 'application/x-troff-ms',
-        'msg' => 'application/vnd.ms-outlook',
+        'msg' => MimeTypesEnum::MIME_APPLICATION_OUTLOOK,
         'mvb' => 'application/x-msmediaview',
         'nc' => 'application/x-netcdf',
-        'nws' => 'message/rfc822',
+        'nws' => MimeTypesEnum::MIME_TEXT_RFC822,
         'oda' => 'application/oda',
         'odt' => 'application/vnd.oasis.opendocument.text',
         'p10' => 'application/pkcs10',
@@ -188,8 +195,8 @@ class FileHelper_MimeTypes
         'p7m' => 'application/x-pkcs7-mime',
         'p7r' => 'application/x-pkcs7-certreqresp',
         'p7s' => 'application/x-pkcs7-signature',
-        'pbm' => 'image/x-portable-bitmap',
-        'pdf' => 'application/pdf',
+        'pbm' => MimeTypesEnum::MIME_IMAGE_PBM,
+        'pdf' => MimeTypesEnum::MIME_APPLICATION_PDF,
         'pfx' => 'application/x-pkcs12',
         'pgm' => 'image/x-portable-graymap',
         'pko' => 'application/ynd.ms-pkipko',
@@ -198,7 +205,7 @@ class FileHelper_MimeTypes
         'pml' => 'application/x-perfmon',
         'pmr' => 'application/x-perfmon',
         'pmw' => 'application/x-perfmon',
-        'png' => 'image/png',
+        'png' => MimeTypesEnum::MIME_IMAGE_PNG,
         'pnm' => 'image/x-portable-anymap',
         'pot' => 'application/vnd.ms-powerpoint',
         'ppm' => 'image/x-portable-pixmap',
@@ -207,56 +214,56 @@ class FileHelper_MimeTypes
         'prf' => 'application/pics-rules',
         'ps' => 'application/postscript',
         'pub' => 'application/x-mspublisher',
-        'qt' => 'video/quicktime',
+        'qt' => MimeTypesEnum::MIME_VIDEO_QUICKTIME,
         'ra' => 'audio/x-pn-realaudio',
         'ram' => 'audio/x-pn-realaudio',
         'ras' => 'image/x-cmu-raster',
         'rgb' => 'image/x-rgb',
-        'rmi' => 'audio/mid',
+        'rmi' => MimeTypesEnum::MIME_AUDIO_MID,
         'roff' => 'application/x-troff',
-        'rtf' => 'application/rtf',
-        'rtx' => 'text/richtext',
-        'sql' => 'application/sql',
+        'rtf' => MimeTypesEnum::MIME_APPLICATION_RTF,
+        'rtx' => MimeTypesEnum::MIME_TEXT_RICHTEXT,
+        'sql' => MimeTypesEnum::MIME_APPLICATION_SQL,
         'scd' => 'application/x-msschedule',
         'sct' => 'text/scriptlet',
-        'ser' => 'text/plain',
-        'bak' => 'application/zip',
+        'ser' => MimeTypesEnum::MIME_TEXT_PLAIN,
+        'bak' => MimeTypesEnum::MIME_ARCHIVE_ZIP,
         'setpay' => 'application/set-payment-initiation',
         'setreg' => 'application/set-registration-initiation',
         'sh' => 'application/x-sh',
         'shar' => 'application/x-shar',
         'sit' => 'application/x-stuffit',
-        'snd' => 'audio/basic',
+        'snd' => MimeTypesEnum::MIME_AUDIO_BASIC,
         'spc' => 'application/x-pkcs7-certificates',
         'spl' => 'application/futuresplash',
         'src' => 'application/x-wais-source',
         'sst' => 'application/vnd.ms-pkicertstore',
         'stl' => 'application/vnd.ms-pkistl',
-        'stm' => 'text/html',
+        'stm' => MimeTypesEnum::MIME_TEXT_HTML,
         'sv4cpio' => 'application/x-sv4cpio',
         'sv4crc' => 'application/x-sv4crc',
-        'svg' => 'image/svg+xml',
+        'svg' => MimeTypesEnum::MIME_IMAGE_SVG,
         'swf' => 'application/x-shockwave-flash',
         't' => 'application/x-troff',
-        'tar' => 'application/x-tar',
+        'tar' => MimeTypesEnum::MIME_ARCHIVE_TAR,
         'tcl' => 'application/x-tcl',
         'tex' => 'application/x-tex',
         'texi' => 'application/x-texinfo',
         'texinfo' => 'application/x-texinfo',
-        'tgz' => 'application/x-compressed',
-        'tif' => 'image/tiff',
-        'tiff' => 'image/tiff',
-        'ttf' => 'font/ttf',
+        'tgz' => MimeTypesEnum::MIME_ARCHIVE_TGZ,
+        'tif' => MimeTypesEnum::MIME_IMAGE_TIFF,
+        'tiff' => MimeTypesEnum::MIME_IMAGE_TIFF,
+        'ttf' => MimeTypesEnum::MIME_FONT_TTF,
         'tr' => 'application/x-troff',
         'trm' => 'application/x-msterminal',
         'tsv' => 'text/tab-separated-values',
-        'txt' => 'text/plain',
+        'txt' => MimeTypesEnum::MIME_TEXT_PLAIN,
         'uls' => 'text/iuls',
         'ustar' => 'application/x-ustar',
         'vcf' => 'text/x-vcard',
         'vrml' => 'x-world/x-vrml',
-        'wav' => 'audio/x-wav',
-        'webm' => 'video/webm',
+        'wav' => MimeTypesEnum::MIME_AUDIO_WAV,
+        'webm' => MimeTypesEnum::MIME_VIDEO_WEBM,
         'wcm' => 'application/vnd.ms-works',
         'wdb' => 'application/vnd.ms-works',
         'wks' => 'application/vnd.ms-works',
@@ -267,35 +274,55 @@ class FileHelper_MimeTypes
         'wrz' => 'x-world/x-vrml',
         'xaf' => 'x-world/x-vrml',
         'xbm' => 'image/x-xbitmap',
-        'xla' => 'application/vnd.ms-excel',
-        'xlc' => 'application/vnd.ms-excel',
-        'xlm' => 'application/vnd.ms-excel',
-        'xls' => 'application/vnd.ms-excel',
+        'xla' => MimeTypesEnum::MIME_APPLICATION_EXCEL,
+        'xlc' => MimeTypesEnum::MIME_APPLICATION_EXCEL,
+        'xlm' => MimeTypesEnum::MIME_APPLICATION_EXCEL,
+        'xls' => MimeTypesEnum::MIME_APPLICATION_EXCEL,
         'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        'xlt' => 'application/vnd.ms-excel',
-        'xlw' => 'application/vnd.ms-excel',
+        'xlt' => MimeTypesEnum::MIME_APPLICATION_EXCEL,
+        'xlw' => MimeTypesEnum::MIME_APPLICATION_EXCEL,
         'xof' => 'x-world/x-vrml',
         'xpm' => 'image/x-xpixmap',
         'xwd' => 'image/x-xwindowdump',
-        'xml' => 'text/xml',
-        'z' => 'application/x-compress',
-        '7z' => 'application/x-7z-compressed',
-        'zip' => 'application/zip'
+        'xml' => MimeTypesEnum::MIME_TEXT_XML,
+        'z' => MimeTypesEnum::MIME_ARCHIVE_Z,
+        '7z' => MimeTypesEnum::MIME_ARCHIVE_7Z,
+        'zip' => MimeTypesEnum::MIME_ARCHIVE_ZIP
     );
 
     /**
+     * Resets all mime types and browser extensions to
+     * the default values.
+     *
+     * @return void
+     */
+    public static function resetToDefaults() : void
+    {
+        self::$mimeTypes = self::DEFAULT_MIME_TYPES;
+        self::$browserExtensions = self::DEFAULT_BROWSER_EXTENSIONS;
+    }
+
+    /**
      * Retrieves the mime type for the specified extension.
-     * @param string $extension
+     *
+     * @param string $extension Case-insensitive extension, with or without a leading dot.
      * @return string|NULL
      */
     public static function getMime(string $extension) : ?string
     {
+        $extension = self::filterExtension($extension);
+
         if (isset(self::$mimeTypes[$extension]))
         {
             return self::$mimeTypes[$extension];
         }
 
         return null;
+    }
+
+    private static function filterExtension(string $extension) : string
+    {
+        return ltrim(strtolower($extension), '.');
     }
 
     /**
@@ -307,43 +334,93 @@ class FileHelper_MimeTypes
      */
     public static function canBrowserDisplay(string $extension) : bool
     {
-        return in_array($extension, self::$browserExtensions);
+        return in_array(self::filterExtension($extension), self::$browserExtensions);
     }
 
     /**
-     * Register custom extensions and related mimetypes
-     * First check array is there any defined extension, if not register custom extension
+     * Sets whether the browser can display files with the
+     * specified extension natively.
      *
      * @param string $extension
-     * @param string $mimeType
-     * @return bool
+     * @param bool $canDisplay
+     * @return void
      */
-    public static function registerCustom(string $extension, string $mimeType) : bool
+    public static function setBrowserCanDisplay(string $extension, bool $canDisplay) : void
     {
-        if (!isset(self::$mimeTypes[$extension]))
+        $extension = self::filterExtension($extension);
+
+        if($canDisplay && !in_array($extension, self::$browserExtensions))
         {
-            self::$mimeTypes[$extension] = $mimeType;
-            return true;
+            self::$browserExtensions[] = $extension;
         }
 
-        return false;
+        if(!$canDisplay && in_array($extension, self::$browserExtensions))
+        {
+            $key = array_search($extension, self::$browserExtensions);
+            unset(self::$browserExtensions[$key]);
+        }
     }
 
     /**
-     * Change mime type of existing extension if there is defined extension
+     * Register custom extensions and related mimetypes.
      *
      * @param string $extension
      * @param string $mimeType
-     * @return bool
+     * @return string|NULL The old mime type, or NULL if none was set.
+     * @deprecated Use {@see setMimeType()} instead.
      */
-    public static function setMimeType(string $extension, string $mimeType) : bool
+    public static function registerCustom(string $extension, string $mimeType) : ?string
     {
-        if (isset(self::$mimeTypes[$extension]))
+        return self::setMimeType($extension, $mimeType);
+    }
+
+    /**
+     * Add a mime type, or overwrite the type of existing extensions.
+     *
+     * @param string $extension
+     * @param string $mimeType
+     * @return string|NULL The old mime type, or NULL if none was set.
+     */
+    public static function setMimeType(string $extension, string $mimeType) : ?string
+    {
+        $extension = self::filterExtension($extension);
+
+        $old = self::$mimeTypes[$extension] ?? null;
+
+        self::$mimeTypes[$extension] = strtolower($mimeType);
+
+        return $old;
+    }
+
+    public static function extensionExists(string $extension) : bool
+    {
+        return isset(self::$mimeTypes[self::filterExtension($extension)]);
+    }
+
+    /**
+     * Attempts to find the extensions for the specified mime type.
+     *
+     * Note: Returns a list of extensions, as mime types can have
+     * multiple extensions (like {@see FileHelper\MimeTypesEnum::MIME_APPLICATION_EXCEL}).
+     *
+     * @param string $mime E.g. "image/jpeg"
+     * @return string[] List of extensions matching the mime type, or an empty array if none were found.
+     */
+    public static function getExtensionsByMime(string $mime) : array
+    {
+        $mime = strtolower($mime);
+        $result = array();
+
+        foreach(self::$mimeTypes as $extension => $mimeType)
         {
-            self::$mimeTypes[$extension] = $mimeType;
-            return true;
+            if($mime === $mimeType)
+            {
+                $result[] = $extension;
+            }
         }
 
-        return false;
+        sort($result);
+
+        return $result;
     }
 }
