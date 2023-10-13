3a7db3ede7a72c53e99c9de2b8592d480593aa73
Mistralys <s.mordziol@gmail.com>
2023-10-13 17:53:22 +0200

    Further changes to split the core classes.

--- src/XMLHelper/Exception.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace AppUtils;
-
-class XMLHelper_Exception extends BaseException
-{
-}
--- src/functions.php
+++ src/functions.php
@@ -5,36 +5,6 @@ declare(strict_types=1);
 namespace AppUtils;
 
 /**
- * Parses the specified number, and returns a NumberInfo instance.
- *
- * @param NumberInfo|string|int|float|NULL $value
- * @param bool $forceNew
- * @return NumberInfo
- */
-function parseNumber($value, bool $forceNew=false) : NumberInfo
-{
-    if($value instanceof NumberInfo && $forceNew !== true) {
-        return $value;
-    }
-    
-    return new NumberInfo($value);
-}
-
-/**
- * Like {@see parseNumber()}, but returns an immutable
- * instance where any operations that modify the value
- * return a new instance, leaving the original instance
- * intact.
- *
- * @param NumberInfo|string|int|float|NULL $value
- * @return NumberInfo_Immutable
- */
-function parseNumberImmutable($value) : NumberInfo_Immutable
-{
-    return new NumberInfo_Immutable($value);
-}
-
-/**
  * Creates a boolean value.
  * 
  * @param bool $initial The initial boolean value to use.
@@ -78,20 +48,4 @@ function valBoolFalse(bool $initial=true) : Value_Bool_False
 function isCLI() : bool
 {
     return PHP_SAPI === "cli";
-}
-
-/**
- * Removes the specified values from the target array.
- *
- * @param array<mixed> $haystack
- * @param array<mixed> $values
- * @param bool $strict
- * @return array<mixed>
- */
-function array_remove_values(array $haystack, array $values, bool $strict=true) : array
-{
-    return array_filter(
-        $haystack,
-        static fn($entry) => !in_array($entry, $values, $strict)
-    );
 }
