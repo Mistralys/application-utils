37bc16c807e04e3639c1730d93a52da0f7673c68
Mistralys <s.mordziol@gmail.com>
2023-11-28 16:43:27 +0100

    Removed the deprecated SVN helper.

--- src/SVNHelper/Target/File.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace AppUtils;
-
-class SVNHelper_Target_File extends SVNHelper_Target
-{
-}
--- src/SVNHelper/Target/Folder.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace AppUtils;
-
-class SVNHelper_Target_Folder extends SVNHelper_Target
-{
-}
